3080aebd03627c29b43ec30fcc08817c46c73242
jethers <jethinho@gmail.com>
2021-03-04 14:50:59 -0300

    Quarto commit página php

--- index.php
+++ index.php
@@ -7,5 +7,9 @@ Burger do Zé
 
 Teste 123 
 
+<BR>
+
+<img src="https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2020/05/BierFass-Lago-hamb%C3%BArguer-de-Picanha-cr%C3%A9dito-Inove-Aceleradora-1000x600.jpg">
+
 </body>
 </html>
