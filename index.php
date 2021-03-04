443065e75779724cccb5078f970e26a33b40d4d5
jethers <jethinho@gmail.com>
2021-03-04 15:15:46 -0300

    style

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ Teste 123
 
 <BR>
 
-<img src="https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2020/05/BierFass-Lago-hamb%C3%BArguer-de-Picanha-cr%C3%A9dito-Inove-Aceleradora-1000x600.jpg">
+<img src="https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2020/05/BierFass-Lago-hamb%C3%BArguer-de-Picanha-cr%C3%A9dito-Inove-Aceleradora-1000x600.jpg" 
+style {
+    width: 600;
+}>
 
 </body>
 </html>
